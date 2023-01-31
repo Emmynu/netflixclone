f03b6d3c637c3e61ed924dcf08194d0e838437e4
Emmy nuel <118024691+Emmynu@users.noreply.github.com>
2023-01-31 19:14:07 +0100

    Add files via upload

---
+++ netflix.js
@@ -0,0 +1,87 @@
+function faqs(){
+     document.getElementById("answer").style.display = "block"
+     document.getElementById("plus").textContent = "X"
+     document.getElementById("plus").style.transition = ".90000s"
+     document.getElementById("answer").style.transition = ".90000s"
+}
+
+function dbl(){
+    document.getElementById("answer").style.display = "none"
+    document.getElementById("plus").textContent = "+"
+    document.getElementById("plus").style.transition = ".90000s"
+    document.getElementById("answer").style.transition = ".90000s"
+}
+
+
+function faqs2(){
+    document.getElementById("answer2").style.display = "block"
+    document.getElementById("plus2").textContent = "X"
+    document.getElementById("plus2").style.transition =".90000s"
+    document.getElementById("answer2").style.transition = ".90000s"
+}
+
+function dbl2(){
+   document.getElementById("answer2").style.display = "none"
+   document.getElementById("plus2").textContent = "+"
+   document.getElementById("plus2").style.transition = ".90000s"
+   document.getElementById("answer2").style.transition = ".90000s"
+}
+
+function faqs3(){
+    document.getElementById("answer3").style.display = "block"
+    document.getElementById("plus3").textContent = "X"
+    document.getElementById("plus3").style.transition =".90000s"
+    document.getElementById("answer3").style.transition = ".90000s"
+}
+
+function dbl3(){
+   document.getElementById("answer3").style.display = "none"
+   document.getElementById("plus3").textContent = "+"
+   document.getElementById("plus3").style.transition = ".90000s"
+   document.getElementById("answer3").style.transition = ".90000s"
+}
+
+function faqs4(){
+    document.getElementById("answer4").style.display = "block"
+    document.getElementById("plus4").textContent = "X"
+    document.getElementById("plus4").style.transition =".90000s"
+    document.getElementById("answer4").style.transition = ".90000s"
+}
+
+function dbl4(){
+   document.getElementById("answer4").style.display = "none"
+   document.getElementById("plus4").textContent = "+"
+   document.getElementById("plus4").style.transition = ".90000s"
+   document.getElementById("answer4").style.transition = ".90000s"
+}
+
+
+function faqs5(){
+    document.getElementById("answer5").style.display = "block"
+    document.getElementById("plus5").textContent = "X"
+    document.getElementById("plus5").style.transition =".90000s"
+    document.getElementById("answer5").style.transition = ".90000s"
+}
+
+function dbl5(){
+   document.getElementById("answer5").style.display = "none"
+   document.getElementById("plus5").textContent = "+"
+   document.getElementById("plus5").style.transition = ".90000s"
+   document.getElementById("answer5").style.transition = ".90000s"
+}
+
+
+function faqs6(){
+    document.getElementById("answer6").style.display = "block"
+    document.getElementById("plus6").textContent = "X"
+    document.getElementById("plus6").style.transition =".90000s"
+    document.getElementById("answer6").style.transition = ".90000s"
+}
+
+function dbl6(){
+   document.getElementById("answer6").style.display = "none"
+   document.getElementById("plus6").textContent = "+"
+   document.getElementById("plus6").style.transition = ".90000s"
+   document.getElementById("answer6").style.transition = ".90000s"
+}
+
